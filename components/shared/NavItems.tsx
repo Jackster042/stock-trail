3cc1c2ef4665f3043516598fbf024db5e1197044
Nemanja Stojanovic <feemail042@gmail.com>
2025-10-27 23:58:19 +0100

    Implement search stock option

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -4,8 +4,13 @@ import React from "react";
 import { NAV_ITEMS as navItems } from "@/lib/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import SearchCommand from "../SearchCommand";
 
-const NavItems = () => {
+const NavItems = ({
+  initialStocks,
+}: {
+  initialStocks: StockWithWatchlistStatus[];
+}) => {
   const pathname = usePathname();
 
   const isActive = (path: string) => {
@@ -15,18 +20,39 @@ const NavItems = () => {
 
   return (
     <ul className="flex flex-col gap-3 p-2 font-medium sm:gap-10 sm:flex-row">
-      {navItems.map(({ href, label }) => (
-        <li key={href}>
-          <Link
-            href={href}
-            className={`hover:text-yellow-400 transition-colors
+      {navItems.map(({ href, label }) => {
+        if (label === "Search")
+          return (
+            <li key="search-trigger">
+              <SearchCommand
+                renderAs="text"
+                label="Search"
+                // initialStocks={[
+                //   {
+                //     symbol: "AAPL",
+                //     name: "Apple",
+                //     exchange: "NASDAQ",
+                //     type: "stock",
+                //   },
+                // ]}
+                initialStocks={initialStocks}
+              />
+            </li>
+          );
+
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              className={`hover:text-yellow-400 transition-colors
                 ${isActive(href) ? "text-yellow-100" : ""}
                 `}
-          >
-            {label}
-          </Link>
-        </li>
-      ))}
+            >
+              {label}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
