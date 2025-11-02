16156bce87fb74c3a54b8ba623e262b8c311a737
Nemanja Stojanovic <feemail042@gmail.com>
2025-11-02 23:17:27 +0100

    deployment fix

--- components/WatchlistButton.tsx
+++ components/WatchlistButton.tsx
@@ -61,22 +61,6 @@ const WatchlistButton = ({
       toast.error("Unable to update watchlist", { description: String(error) });
       console.error(`Error toggling watchlist for ${symbol}`, error);
     }
-
-    // const result = added
-    //   ? await removeFromWatchlist(symbol)
-    //   : await addToWatchlist(symbol, company);
-
-    // if (result.success) {
-    //   toast.success(
-    //     added ? "Stock removed from watchlist" : "Stock added to watchlist",
-    //     {
-    //       description: `${company} ${
-    //         added ? "removed" : "added"
-    //       } from watchlist`,
-    //     }
-    //   );
-    // }
-    // onWatchlistChange?.(symbol, !added);
   };
 
   const debounceToggle = useDebounce(toggleWatchlist, 500);
@@ -115,23 +99,7 @@ const WatchlistButton = ({
       className={`watchlist-btn ${added ? "watchlist-remove" : ""}`}
       onClick={handleClick}
     >
-      {showTrashIcon && added ? (
-        // <svg
-        //   xmlns="http://www.w3.org/2000/svg"
-        //   fill="none"
-        //   viewBox="0 0 24 24"
-        //   strokeWidth={1.5}
-        //   stroke="currentColor"
-        //   className="w-5 h-5 mr-2"
-        // >
-        //   <path
-        //     strokeLinecap="round"
-        //     strokeLinejoin="round"
-        //     d="M6 7h12M9 7V5a1 1 0 011-1h4a1 1 0 011 1v2m-7 4v6m4-6v6m4-6v6"
-        //   />
-        // </svg>
-        <Trash2 />
-      ) : null}
+      {showTrashIcon && added ? <Trash2 /> : null}
       <span>{label}</span>
     </button>
   );
